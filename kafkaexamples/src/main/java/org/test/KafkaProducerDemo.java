31b66a886e5a015f5906bcd6cd418c337324e381
Christof Reich <christof.reich@sap.com>
2018-06-21 13:48:51 +0200

    generic settings

--- kafkaexamples/src/main/java/org/test/KafkaProducerDemo.java
+++ kafkaexamples/src/main/java/org/test/KafkaProducerDemo.java
@@ -1,3 +1,5 @@
+package org.test;
+
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
